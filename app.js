020e112f309cb9309dfe3a1623e3834b2712a6fe
Yardimo <yardimo.app@gmail.com>
2025-08-11 22:10:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// Your Mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlrZW5vcnJpcyIsImEiOiJjbWU0eXIyMHQwb3NlMm1wcHNkYXRwYzYxIn0.HrNdh-cFPgqR4SSYgkdHTw';
 
+// Initialize the map
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v12',
+  style: 'mapbox://styles/mapbox/streets-v11',
   center: [-81.2497, 42.9836], // London, Ontario
   zoom: 12
 });
 
-map.addControl(new mapboxgl.NavigationControl());
+// Add navigation controls (zoom in/out)
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
